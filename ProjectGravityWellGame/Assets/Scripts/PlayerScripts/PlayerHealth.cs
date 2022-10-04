fcc453d6112d31f55f2834b504662fcf2e0579aa
006437570 <006437570@coyote.csusb.edu>
2022-10-04 14:18:42 -0700

    Added playerHealth.cs

---
+++ ProjectGravityWellGame/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int currentHealth = 2; //subject to change
+
+    // Death animation effect goes here when ready :)
+    //public GameObject deathAni;
+
+    // Will calculate by subtracting currentHealth by damage amount
+    public void calcDmg (int dmg)
+    {
+        currentHealth -= dmg;
+
+        // If the players health hits zero or less, then player dies
+        if (currentHealth <= 0)
+        {
+            playerDeath();
+        }
+    }
+
+    void playerDeath()
+    {
+        // Plays player death animation when ready
+        // Instantiate(deathAni, transform.position, Quaternion.identity);
+        
+        Destroy(gameObject);
+    }
+}
