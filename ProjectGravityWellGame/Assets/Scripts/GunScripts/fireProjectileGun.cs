e3529359eeb4540952a58980c253f9d9e64f3d17
006437570 <006437570@coyote.csusb.edu>
2022-10-24 03:14:36 -0700

    fixes

--- ProjectGravityWellGame/Assets/Scripts/GunScripts/fireProjectileGun.cs
+++ ProjectGravityWellGame/Assets/Scripts/GunScripts/fireProjectileGun.cs
@@ -7,9 +7,25 @@ public class fireProjectileGun : MonoBehaviour
     public Transform firepoint; //point from gun that bullet is shot from
     public GameObject bulletPrefab; //bullet prefab that will be shot
 
+    public int magazineSize;
+    public int ammo;
+
+    void Start()
+    {
+        ammo = magazineSize;
+    }
+
     public void Shoot(GameObject playerAttacker)
     {   
-        bulletPrefab.GetComponent<bulletProjectile>().playerAttacker = playerAttacker;
-        Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        if (ammo > 0)
+        {
+            bulletPrefab.GetComponent<bulletProjectile>().playerAttacker = playerAttacker;
+            Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+            ammo--;
+        }
+        else
+        {
+            //audio
+        }
     }
 }
