f3745a561f334223f07ab57e29a6216ab89bc7e7
ChodyFlims <006185370@coyote.csusb.edu>
2022-11-08 19:17:38 -0800

    Added Resolution Changer haven't tested the build might not work :P

--- ProjectGravityWellGame/Assets/Scripts/MainMenu.cs
+++ ProjectGravityWellGame/Assets/Scripts/MainMenu.cs
@@ -17,4 +17,20 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+    List<int> widths = new List<int>() {320, 540, 1280, 1920};
+    List<int> heights = new List<int>() {568, 960, 800, 1080};
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
+    public void SetFullScreen(bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
+    }
+
 }
